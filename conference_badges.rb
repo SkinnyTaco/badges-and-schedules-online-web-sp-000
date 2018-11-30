8d0c1946f8a0b52cad8a468aebd54b4896c61559
Daniel Dawson <dannyldawson@gmail.com>
2018-11-30 05:26:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def batch_badge_creator(names_array)
+  messages_array = []
+  names_array.each do |name|
+    messages_array << "Hello, my name is #{name}"
+  end
+  messages_array
+end
+
+def assign_rooms(speakers_array)
+  
+end
